74df19745bce9a18efa851144ea7818adc2e1934
jelleree <97703492+jelleree@users.noreply.github.com>
2025-10-28 16:38:19 +0100

    Update main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Verkiezingspool2025 from './Verkiezingspool2025'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Verkiezingspool2025 />
+  </React.StrictMode>,
+)
